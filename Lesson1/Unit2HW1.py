8491fe1fbbc4ed9a8f65cb537bc5a3a66eee0918
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-03-06 09:52:40 -0800

    update

--- Lesson1/Unit2HW1.py
+++ Lesson1/Unit2HW1.py
@@ -8,19 +8,37 @@ class restaurant:
     """A simple attempt to model a dog."""
 
     def __init__(self, restaurant_name: str, cuisine_type: str) -> None:
-        """Creates a new dog object
+        """creates a restaurant name and describes the cuisine type
 
         Args:
-            name (str): Name of dog
-            age (int): Age of dog
+            restaurant_name (str): Name of restaurant
+            cuisine_type (int): Style of cuisine
         """
-        self.name = name
-        self.age = age
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
 
-    def sit(self) -> None:
+    def describe_restaurant(self) -> None:
         """Simulate a dog sitting in response to a command."""
-        print(f"{self.name} is now sitting.")
+        print(f"The restaurants name is {self.restaurant_name} ")
+        print(f"The restaurants cuisine is {self.cuisine_type}.")
 
-    def roll_over(self) -> None:
-        """Simulate rolling over in response to a command."""
-        print(f"{self.name} rolled over!")
+    def open_restaurant(self) -> None:
+        """Defines when the restaurant is open"""
+        print(f"{self.restaurant_name} is open.")
+
+def main():
+    restaurant1 = restaurant("Velucci's","Italian")
+    restaurant2 = restaurant("Pine Hill","Brewery")
+    restaurant3 = restaurant("The Soup Bar", "Soup")
+    
+    restaurant1.describe_restaurant()
+    restaurant1.open_restaurant()
+    print(" ")
+    restaurant2.describe_restaurant()
+    restaurant2.open_restaurant()
+    print(" ")
+    restaurant3.describe_restaurant()
+    restaurant3.open_restaurant()
+    
+if __name__ == "__main__":
+    main()
