80ee25ca469ce8c301e25d281b2b5adb7ba4c144
Zachary Briggs <briggsz@wlhs.wlwv.k12.or.us>
2025-03-10 21:14:56 -0700

    Update Unit2HW2.py
    commit

--- Lesson1/Unit2HW2.py
+++ Lesson1/Unit2HW2.py
@@ -42,3 +42,53 @@ if __name__ == "__main__":
     main()
 
 # self.number_served+= number_served
+################################
+class user:
+    """an attempt to create and describe a user"""
+
+    def __init__(self, first_name: str, last_name: str, age: int, hair_color: str, login_attempts:int) -> None:
+        """creates the users first and last name, age, hair color
+
+        Args:
+            first_name (str): first name of person
+            last_name (str): last name of person
+            age (int): age of the person
+            hair_color (str): persons hair color
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.hair_color = hair_color
+    def increment_login_attempts(self)->None:
+        self.login_attempts+=1
+    
+    def describe_user(self) -> None:
+        """Describes the restaurant, first name, last name and description"""
+        print(f"This persons name is {self.first_name} {self.last_name}")
+        print(f"They are {self.age} years old and have {self.hair_color} hair")
+
+    def greeting1(self) -> None:
+        """greets the customer"""
+        print(f"Welcome {self.first_name}.")
+    def greeting2(self) -> None:
+        """greets the customer"""
+        print(f"Good evening {self.first_name}.")
+    def greeting3(self) -> None:
+        """greets the customer"""
+        print(f"Hello {self.first_name} welcome in")
+
+def user_main():
+    user1 = user("Tom", "Brady", 47, "brown")
+    user2 = user("Anne","Johnson", 34, "blonde")
+    user3 = user("Dwayne", "Roberts", 28, "black")
+    
+    user1.describe_user()
+    user1.greeting1()
+    user1.increment_login_attempts(2)
+    print(" ")
+    user2.describe_user()
+    user2.greeting2()
+    print(" ")
+    user3.describe_user()
+    user3.greeting3()
+
