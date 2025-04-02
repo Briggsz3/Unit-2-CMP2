53f7d702f042f2e187dacedd6fa328ec8d34c15b
briggsz07 <briggsz@wlhs.wlwv.k12.or.us>
2025-04-02 09:28:23 -0700

    commit

--- Lesson7/ElectricCar.py
+++ Lesson7/ElectricCar.py
@@ -1,2 +1,6 @@
-class ElectricCar():
-    pass
+import Car
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year, battery_level):
+        super().__init__(make, model, year) # can sub in things like "Nissan" for "make" which would make all electric cars Nissans
+        self.battery_level = battery_level
